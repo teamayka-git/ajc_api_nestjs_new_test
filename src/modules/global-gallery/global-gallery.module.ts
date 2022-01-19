01508612323da636c7d35f768df1fc6d71416207
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-01-20 01:03:32 +0530

    global gallery done 1

--- src/modules/global-gallery/global-gallery.module.ts
+++ src/modules/global-gallery/global-gallery.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { GlobalGalleryService } from './global-gallery.service';
 import { GlobalGalleryController } from './global-gallery.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ModelNames } from 'src/common/model_names';
+import { GlobalGalleriesSchema } from 'src/tableModels/globalGalleries.model';
 
 @Module({
+  imports:[MongooseModule.forFeature([{name:ModelNames.GLOBAL_GALLERIES,schema:GlobalGalleriesSchema}])],
   controllers: [GlobalGalleryController],
   providers: [GlobalGalleryService]
 })
