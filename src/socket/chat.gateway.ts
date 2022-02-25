abf8187bcc7653271243b64a0d166dd1a968b749
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-02-25 18:10:23 +0530

    socket port 4001

--- src/socket/chat.gateway.ts
+++ src/socket/chat.gateway.ts
@@ -25,7 +25,7 @@ import { UploadedFileDirectoryPath } from 'src/common/uploaded_file_directory_pa
 import { StringUtils } from 'src/utils/string_utils';
 import { ThumbnailUtils } from 'src/utils/ThumbnailUtils';
 
-@WebSocketGateway({ cors: true }) //todo add port from env file
+@WebSocketGateway(4001,{ cors: true }) //todo add port from env file
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
