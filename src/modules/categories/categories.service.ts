962ac980d8d2391ddcaf247c7603ad7fad0d0c8c
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-03-31 20:50:41 +0530

    category edit for db deep test 6

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -126,10 +126,10 @@ export class CategoriesService {
       await this.globalGalleryModel.insertMany(arrayGlobalGalleries, {
         session: transactionSession,
       });
-
-      const responseJSON = { message: 'success', data: { list: result1 } };
+     
+      const responseJSON =   { message: 'success', data: { list: result1 } };
       if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
+        process.env.RESPONSE_RESTRICT == "true" &&
         JSON.stringify(responseJSON).length >=
           GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
       ) {
@@ -231,9 +231,10 @@ export class CategoriesService {
         { new: true, session: transactionSession },
       );
 
-      const responseJSON = { message: 'success', data: result };
+    
+      const responseJSON =  { message: 'success', data: result };
       if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
+        process.env.RESPONSE_RESTRICT == "true" &&
         JSON.stringify(responseJSON).length >=
           GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
       ) {
@@ -272,9 +273,10 @@ export class CategoriesService {
         { new: true, session: transactionSession },
       );
 
+     
       const responseJSON = { message: 'success', data: result };
       if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
+        process.env.RESPONSE_RESTRICT == "true" &&
         JSON.stringify(responseJSON).length >=
           GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
       ) {
@@ -389,39 +391,39 @@ export class CategoriesService {
           },
         );
       }
-      arrayAggregation.push(
-        {
-          $lookup: {
-            from: ModelNames.USER,
-            let: { userId: '$_createdUserId' },
-            pipeline: [
-              {
-                $match: {
-                  $expr: {
-                       $eq: [dto.skip, -1]
-                    
-                  },
-                },
-              },
-              {
-                $match: {
-                  _status: 1,
-                  $expr: {
-                     $eq: ['$_id', '$$userId'] 
-                  },
-                },
-              },
-            ],
-            as: 'userDetails',
-          },
-        },
-        {
-          $unwind: {
-            path: '$userDetails',
-            preserveNullAndEmptyArrays: true,
-          },
-        },
-      );
+
+
+arrayAggregation.push({
+  $switch:{
+    branches:[
+      { case: { $eq: [ dto.skip, -1 ] }, then: {
+            $lookup: {
+              from: ModelNames.USER,
+              let: { userId: '$_createdUserId' },
+              pipeline: [{ $match: {_status:1, $expr: {$and:[{$eq: [dto.skip, -1]},{$eq: ['$_id', '$$userId']}]       } } },],
+              as: 'userDetails',
+            },
+          } }
+    ]
+  }
+});
+
+      // arrayAggregation.push(
+      //   {
+      //     $lookup: {
+      //       from: ModelNames.USER,
+      //       let: { userId: '$_createdUserId' },
+      //       pipeline: [{ $match: {_status:1, $expr: {$and:[{$eq: [dto.skip, -1]},{$eq: ['$_id', '$$userId']}]       } } },],
+      //       as: 'userDetails',
+      //     },
+      //   },
+      //   {
+      //     $unwind: {
+      //       path: '$userDetails',
+      //       preserveNullAndEmptyArrays: true,
+      //     },
+      //   },
+      // );
       var result = await this.categoriesModel
         .aggregate(arrayAggregation)
         .session(transactionSession);
@@ -453,12 +455,13 @@ export class CategoriesService {
         }
       }
 
-      const responseJSON = {
+    
+      const responseJSON =  {
         message: 'success',
         data: { list: result, totalCount: totalCount },
       };
       if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
+        process.env.RESPONSE_RESTRICT == "true" &&
         JSON.stringify(responseJSON).length >=
           GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
       ) {
@@ -512,6 +515,11 @@ export class CategoriesService {
         );
       }
 
+
+
+
+
+
       var result = await this.categoriesModel
         .aggregate(arrayAggregation)
         .session(transactionSession);
@@ -543,12 +551,13 @@ export class CategoriesService {
         }
       }
 
-      const responseJSON = {
+     
+      const responseJSON =  {
         message: 'success',
         data: { list: result, totalCount: totalCount },
       };
       if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
+        process.env.RESPONSE_RESTRICT == "true" &&
         JSON.stringify(responseJSON).length >=
           GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
       ) {
@@ -577,12 +586,48 @@ export class CategoriesService {
         .count({ _code: dto.value })
         .session(transactionSession);
 
-      const responseJSON = {
+     
+        const responseJSON =   {
+          message: 'success',
+          data: { count: resultCount },
+        };
+        if (
+          process.env.RESPONSE_RESTRICT == "true" &&
+          JSON.stringify(responseJSON).length >=
+            GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
+        ) {
+          throw new HttpException(
+            GlobalConfig().RESPONSE_RESTRICT_RESPONSE +
+              JSON.stringify(responseJSON).length,
+            HttpStatus.INTERNAL_SERVER_ERROR,
+          );
+        }
+        await transactionSession.commitTransaction();
+        await transactionSession.endSession();
+        return responseJSON;
+    } catch (error) {
+      await transactionSession.abortTransaction();
+      await transactionSession.endSession();
+      throw error;
+    }
+  }
+  
+async checkNameExisting(dto: CheckNameExistDto) {
+  var dateTime = new Date().getTime();
+  const transactionSession = await this.connection.startSession();
+  transactionSession.startTransaction();
+  try {
+    var resultCount = await this.categoriesModel
+      .count({ _name: dto.value,_status:{$in:[1,0]} })
+      .session(transactionSession);
+
+   
+      const responseJSON =  {
         message: 'success',
         data: { count: resultCount },
       };
       if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
+        process.env.RESPONSE_RESTRICT == "true" &&
         JSON.stringify(responseJSON).length >=
           GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
       ) {
@@ -595,44 +640,11 @@ export class CategoriesService {
       await transactionSession.commitTransaction();
       await transactionSession.endSession();
       return responseJSON;
-    } catch (error) {
-      await transactionSession.abortTransaction();
-      await transactionSession.endSession();
-      throw error;
-    }
+  } catch (error) {
+    await transactionSession.abortTransaction();
+    await transactionSession.endSession();
+    throw error;
   }
+}
 
-  async checkNameExisting(dto: CheckNameExistDto) {
-    var dateTime = new Date().getTime();
-    const transactionSession = await this.connection.startSession();
-    transactionSession.startTransaction();
-    try {
-      var resultCount = await this.categoriesModel
-        .count({ _name: dto.value, _status: { $in: [1, 0] } })
-        .session(transactionSession);
-
-      const responseJSON = {
-        message: 'success',
-        data: { count: resultCount },
-      };
-      if (
-        process.env.RESPONSE_RESTRICT == 'true' &&
-        JSON.stringify(responseJSON).length >=
-          GlobalConfig().RESPONSE_RESTRICT_DEFAULT_COUNT
-      ) {
-        throw new HttpException(
-          GlobalConfig().RESPONSE_RESTRICT_RESPONSE +
-            JSON.stringify(responseJSON).length,
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
-      }
-      await transactionSession.commitTransaction();
-      await transactionSession.endSession();
-      return responseJSON;
-    } catch (error) {
-      await transactionSession.abortTransaction();
-      await transactionSession.endSession();
-      throw error;
-    }
-  }
 }
