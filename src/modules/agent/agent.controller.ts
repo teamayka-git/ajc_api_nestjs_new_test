68fb3e78bab6987b783030c07733ca9f193dcc0a
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-04-01 17:35:29 +0530

    updation 3

--- src/modules/agent/agent.controller.ts
+++ src/modules/agent/agent.controller.ts
@@ -1,10 +1,33 @@
-import { Body, Controller, Delete, Post, Put, Request, Res, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Post,
+  Put,
+  Request,
+  Res,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 import { Roles } from 'src/Auth/roles.decorator';
 import { RolesGuard } from 'src/Auth/roles.guard';
-import { GuardUserRole, GuardUserRoleStringGenerate } from 'src/common/GuardUserRole';
-import {  AgentCreateDto, AgentEditDto, AgentListDto, AgentLoginDto, AgentStatusChangeDto, CheckEmailExistDto, CheckMobileExistDto, ListFilterLocadingAgentDto } from './agent.dto';
+import {
+  GuardUserRole,
+  GuardUserRoleStringGenerate,
+} from 'src/common/GuardUserRole';
+import {
+  AgentCreateDto,
+  AgentEditDto,
+  AgentListDto,
+  AgentLoginDto,
+  AgentStatusChangeDto,
+  CheckEmailExistDto,
+  CheckMobileExistDto,
+  ListFilterLocadingAgentDto,
+} from './agent.dto';
 import { AgentService } from './agent.service';
 
 import { Response } from 'express'; //jwt response store in cookie
@@ -13,29 +36,30 @@ import { FileMulterHelper } from 'src/shared/file_multter_helper';
 import { diskStorage } from 'multer';
 @Controller('agent')
 @UseGuards(RolesGuard)
-@ApiTags("Agent Docs") 
+@ApiTags('Agent Docs')
 export class AgentController {
-  constructor( private jwtService: JwtService,private readonly agentService: AgentService) {}
+  constructor(
+    private jwtService: JwtService,
+    private readonly agentService: AgentService,
+  ) {}
 
-  @Post("login")
-  async login(@Body() dto: AgentLoginDto,
+  @Post('login')
+  async login(
+    @Body() dto: AgentLoginDto,
     @Res({ passthrough: true }) response: Response, //jwt response store in cookie
   ) {
     var returnData: Object = await this.agentService.login(dto);
-    
-var userRole=new GuardUserRoleStringGenerate().generate(returnData['_userRole']);
 
-
-
-
-
+    var userRole = new GuardUserRoleStringGenerate().generate(
+      returnData['_userRole'],
+    );
 
     const jwt = await this.jwtService.signAsync(
       {
         _userId_: returnData['_id'],
         _agentId_: returnData['_agentId'],
         _type_: returnData['_type'],
-        _userRole_:userRole
+        _userRole_: userRole,
       },
       { expiresIn: '365d' },
     );
@@ -45,12 +69,11 @@ var userRole=new GuardUserRoleStringGenerate().generate(returnData['_userRole'])
     return { message: 'Success', data: returnData, token: jwt };
   }
 
-
-
-
   @Post()
   @Roles(GuardUserRole.SUPER_ADMIN)
-  @ApiCreatedResponse({ description: 'files upload on these input feilds => [image]' })
+  @ApiCreatedResponse({
+    description: 'files upload on these input feilds => [image]',
+  })
   @UseInterceptors(
     FileFieldsInterceptor(
       [
@@ -66,13 +89,18 @@ var userRole=new GuardUserRoleStringGenerate().generate(returnData['_userRole'])
       },
     ),
   )
-  create(@Body() dto: AgentCreateDto,@Request() req, @UploadedFiles() file) {
-    return this.agentService.create(dto,req["_userId_"],file == null ? {} : JSON.parse(JSON.stringify(file)));
+  create(@Body() dto: AgentCreateDto, @Request() req, @UploadedFiles() file) {
+    return this.agentService.create(
+      dto,
+      req['_userId_'],
+      file == null ? {} : JSON.parse(JSON.stringify(file)),
+    );
   }
-  
+
   @Put()
-  
-  @ApiCreatedResponse({ description: 'files upload on these input feilds => [image]' })
+  @ApiCreatedResponse({
+    description: 'files upload on these input feilds => [image]',
+  })
   @UseInterceptors(
     FileFieldsInterceptor(
       [
@@ -88,34 +116,35 @@ var userRole=new GuardUserRoleStringGenerate().generate(returnData['_userRole'])
       },
     ),
   )
-  edit(@Body() dto: AgentEditDto,@Request() req, @UploadedFiles() file) {
-    return this.agentService.edit(dto,req["_userId_"],file == null ? {} : JSON.parse(JSON.stringify(file)));
+  edit(@Body() dto: AgentEditDto, @Request() req, @UploadedFiles() file) {
+    return this.agentService.edit(
+      dto,
+      req['_userId_'],
+      file == null ? {} : JSON.parse(JSON.stringify(file)),
+    );
   }
   @Delete()
-  status_change(@Body() dto: AgentStatusChangeDto,@Request() req) {
-    return this.agentService.status_change(dto,req["_userId_"]);
+  status_change(@Body() dto: AgentStatusChangeDto, @Request() req) {
+    return this.agentService.status_change(dto, req['_userId_']);
   }
-  
-  @Post("list")
-  list(@Body() dto:AgentListDto) {
+
+  @Post('list')
+  list(@Body() dto: AgentListDto) {
     return this.agentService.list(dto);
   }
 
-  @Post("listFilterLoadingAgent")
-  listFilterLoadingAgent(@Body() dto:ListFilterLocadingAgentDto) {
+  @Post('listFilterLoadingAgent')
+  listFilterLoadingAgent(@Body() dto: ListFilterLocadingAgentDto) {
     return this.agentService.listFilterLoadingAgent(dto);
   }
 
-  @Post("checkEmailExisting")
-  checkEmailExisting(@Body() dto:CheckEmailExistDto) {
+  @Post('checkEmailExisting')
+  checkEmailExisting(@Body() dto: CheckEmailExistDto) {
     return this.agentService.checkEmailExisting(dto);
   }
-  
-  @Post("checkMobileExisting")
-  checkMobileExisting(@Body() dto:CheckMobileExistDto) {
+
+  @Post('checkMobileExisting')
+  checkMobileExisting(@Body() dto: CheckMobileExistDto) {
     return this.agentService.checkMobileExisting(dto);
   }
-  
-
-
 }
--- src/modules/agent/agent.dto.ts
+++ src/modules/agent/agent.dto.ts
@@ -10,38 +10,38 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type, Type as ValidateTypes } from 'class-transformer';
 import { Optional } from '@nestjs/common';
 
-
-const descriptionStatus="0-Inactive, 1-Active, 2-Delete";
-const descriptionListScreenTypeForList="0-total documents count, ,50-populate image global gallery,100-city detail,101-user table details";
-const descriptionListDataGuard="0-edit protect, 1-disabe protect, 2-delete protect";
-const descriptionListGender="0-male, 1-female, 2-other";
-const descriptionListCommisionType="0-Commision Percentage, 1-Commision amount";
-const descriptionListSortOrder="1-ascending, -1-descending";
-const descriptionListSortType="0-Created Date,1-Status  2-Name,3-commision type, 4-UID, 4-";
-const descriptionListScreenTypeForFilterLoading="0-total documents count, 100-city details";
+const descriptionStatus = '0-Inactive, 1-Active, 2-Delete';
+const descriptionListScreenTypeForList =
+  '0-total documents count, ,50-populate image global gallery,100-city detail,101-user table details';
+const descriptionListDataGuard =
+  '0-edit protect, 1-disabe protect, 2-delete protect';
+const descriptionListGender = '0-male, 1-female, 2-other';
+const descriptionListCommisionType =
+  '0-Commision Percentage, 1-Commision amount';
+const descriptionListSortOrder = '1-ascending, -1-descending';
+const descriptionListSortType =
+  '0-Created Date,1-Status  2-Name,3-commision type, 4-UID, 4-';
+const descriptionListScreenTypeForFilterLoading =
+  '0-total documents count, 100-city details';
 export class AgentLoginDto {
- 
   @IsEmail()
   @ApiProperty({})
   email: string;
 
-    @IsString()
-    @ApiProperty({})
-    password: string;
+  @IsString()
+  @ApiProperty({})
+  password: string;
 }
 
-
 export class AgentCreateDto {
-
   @IsString()
   @ApiProperty({})
   name: string;
 
   @Transform(({ value }) => Number(value))
   @IsNumber()
-  @ApiProperty({description:descriptionListGender})
+  @ApiProperty({ description: descriptionListGender })
   gender: number;
-
 
   @IsEmail()
   @ApiProperty({})
@@ -61,46 +61,40 @@ export class AgentCreateDto {
 
   @Transform(({ value }) => Number(value))
   @IsNumber()
-  @ApiProperty({description:descriptionListCommisionType})
+  @ApiProperty({ description: descriptionListCommisionType })
   commisionType: number;
+
   @Transform(({ value }) => Number(value))
   @IsNumber()
   @ApiProperty({})
   commisionAmount: number;
+
   @Transform(({ value }) => Number(value))
   @IsNumber()
   @ApiProperty({})
   commisionPercentage: number;
 
-
-  @Transform(({ value }) =>typeof value == 'string' ? JSON.parse(value) : value    )
+  @Transform(({ value }) =>
+    typeof value == 'string' ? JSON.parse(value) : value,
+  )
   @IsArray()
-  @ApiProperty({ type: [Number],description:descriptionListDataGuard })
-  dataGuard:number[];
-  
-  
+  @ApiProperty({ type: [Number], description: descriptionListDataGuard })
+  dataGuard: number[];
 }
 
-
-
 export class AgentEditDto {
-
-
   @IsString()
   @ApiProperty({})
   agentId: string;
 
-  
   @IsString()
   @ApiProperty({})
   name: string;
 
-  
   @Transform(({ value }) => Number(value))
   @IsNumber()
-  @ApiProperty({description:descriptionListGender})
+  @ApiProperty({ description: descriptionListGender })
   gender: number;
-
 
   @IsString()
   @ApiProperty({})
@@ -110,10 +104,9 @@ export class AgentEditDto {
   @ApiProperty({})
   cityId: string;
 
-
   @Transform(({ value }) => Number(value))
   @IsNumber()
-  @ApiProperty({description:descriptionListCommisionType})
+  @ApiProperty({ description: descriptionListCommisionType })
   commisionType: number;
 
   @Transform(({ value }) => Number(value))
@@ -126,43 +119,41 @@ export class AgentEditDto {
   @ApiProperty({})
   commisionPercentage: number;
 
-
-  @Transform(({ value }) =>typeof value == 'string' ? JSON.parse(value) : value    )
+  @Transform(({ value }) =>
+    typeof value == 'string' ? JSON.parse(value) : value,
+  )
   @IsArray()
-  @ApiProperty({ type: [Number],description:descriptionListDataGuard })
-  dataGuard:number[];
+  @ApiProperty({ type: [Number], description: descriptionListDataGuard })
+  dataGuard: number[];
 }
 
 export class AgentListDto {
   @IsNumber()
-  @ApiProperty({description:descriptionListSortType})
+  @ApiProperty({ description: descriptionListSortType })
   sortType: number;
   @IsNumber()
-  @ApiProperty({description:descriptionListSortOrder})
+  @ApiProperty({ description: descriptionListSortOrder })
   sortOrder: number;
-
-
 
   @IsArray()
   @ArrayMinSize(1)
-  @ApiProperty({ type: [Number],description:descriptionStatus })
-  statusArray:number[];
+  @ApiProperty({ type: [Number], description: descriptionStatus })
+  statusArray: number[];
 
   @IsArray()
-  @ApiProperty({ type: [Number],description:descriptionListScreenTypeForList })
-  screenType:number[];
-  
-  @IsArray()
-  @ApiProperty({ type: [Number],description:descriptionListGender })
-  genders:number[];
-  
-  
+  @ApiProperty({
+    type: [Number],
+    description: descriptionListScreenTypeForList,
+  })
+  screenType: number[];
 
   @IsArray()
-  @ApiProperty({ type: [Number],description:descriptionListCommisionType })
-  commisionType:number[];
-  
-  
+  @ApiProperty({ type: [Number], description: descriptionListGender })
+  genders: number[];
+
+  @IsArray()
+  @ApiProperty({ type: [Number], description: descriptionListCommisionType })
+  commisionType: number[];
 
   @IsArray()
   @ApiProperty({ type: [String] })
@@ -176,67 +167,54 @@ export class AgentListDto {
   @ApiProperty({})
   limit: number;
 
-
   @IsNumber()
   @ApiProperty({})
   skip: number;
 
-
-  
   @IsString()
   @ApiProperty({})
   searchingText: string;
-
 }
 
-
 export class AgentStatusChangeDto {
-
-
   @IsArray()
   @ApiProperty({ type: [String] })
   agentIds: string[];
 
-
   @IsNumber()
-  @ApiProperty({description:descriptionStatus})
+  @ApiProperty({ description: descriptionStatus })
   status: number;
-
 }
 
 export class ListFilterLocadingAgentDto {
   @IsArray()
   @ArrayMinSize(1)
-  @ApiProperty({ type: [Number],description:descriptionStatus })
-  statusArray:number[];
+  @ApiProperty({ type: [Number], description: descriptionStatus })
+  statusArray: number[];
 
   @IsArray()
-  @ApiProperty({ type: [Number],description:descriptionListScreenTypeForFilterLoading })
-  screenType:number[];
-  
+  @ApiProperty({
+    type: [Number],
+    description: descriptionListScreenTypeForFilterLoading,
+  })
+  screenType: number[];
+
   @IsNumber()
   @ApiProperty({})
   limit: number;
 
-
   @IsNumber()
   @ApiProperty({})
   skip: number;
-
 }
 
 export class CheckEmailExistDto {
-  
   @IsString()
   @ApiProperty({})
   value: string;
-
 }
 export class CheckMobileExistDto {
-  
   @IsString()
   @ApiProperty({})
   value: string;
-
 }
-  
