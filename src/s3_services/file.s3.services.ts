16eff4bfd4d819a053a419d15977143be2b646e8
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-04-01 19:55:51 +0530

    s3 bucket test 8

--- src/s3_services/file.s3.services.ts
+++ src/s3_services/file.s3.services.ts
@@ -6,7 +6,10 @@ export class FilesS3Service {
   constructor() {}
 
   public async uploadFile(imageBuffer: Buffer, fileName: string) {
+    console.log('___x1');
     const s3 = new S3();
+    console.log('___x2');
+    console.log('___x3   ' + process.env.BUCKET_NAME);
     return await s3
       .upload({
         Bucket: process.env.BUCKET_NAME!,
