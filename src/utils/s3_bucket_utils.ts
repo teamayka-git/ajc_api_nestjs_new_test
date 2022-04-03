55f10bb41ada87f58e27b02140289c3d7173d2f4
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-04-03 16:22:49 +0530

    3 bucket function create 47

--- src/utils/s3_bucket_utils.ts
+++ src/utils/s3_bucket_utils.ts
@@ -5,7 +5,7 @@ import { v4 as uuidv4 } from 'uuid';
 import * as fs from 'fs';
 
 export class S3BucketUtils {
-  public async uploadMyFile(file: Object, path: String) {
+  public async uploadMyFile(file, path: String) {
     return new Promise(function (resolve, reject) {
       const s3 = new AWS.S3({
         accessKeyId: process.env.AWSAccessKeyID,
@@ -23,7 +23,7 @@ export class S3BucketUtils {
       // let bodyFs = fs.createReadStream(file.path) ;
 
       const params = {
-        ACL: 'public-read',
+        // ACL: 'public-read',
         Bucket: process.env.CDN_BUCKET_NAME,
         Key: new S3BucketUtils().getFileNameGeneratedByCdnBucket(
           file['originalname'],
