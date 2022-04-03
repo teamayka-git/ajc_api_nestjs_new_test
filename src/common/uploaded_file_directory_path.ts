b2590028c08867b55a982826280ed092f547fd24
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-04-03 15:08:46 +0530

    3 bucket function create 34

--- src/common/uploaded_file_directory_path.ts
+++ src/common/uploaded_file_directory_path.ts
@@ -1,4 +1,18 @@
 export enum UploadedFileDirectoryPath {
+  TEMP = 'upload_files/temp/',
+  GLOBAL_GALLERY_CATEGORY = 'upload_files/category/',
+  GLOBAL_GALLERY_SUB_CATEGORY = 'upload_files/subCategory/',
+  GLOBAL_GALLERY_STONE = 'upload_files/stone/',
+  GLOBAL_GALLERY_AGENT = 'upload_files/agent/',
+  GLOBAL_GALLERY_CUSTOMER = 'upload_files/customer/',
+  GLOBAL_GALLERY_BRANCH = 'upload_files/branch/',
+  GLOBAL_GALLERY_EMPLOYEE = 'upload_files/employee/',
+  GLOBAL_GALLERY_SUPPLIER = 'upload_files/supplier/',
+  GLOBAL_GALLERY_OTHERS = 'upload_files/others/',
+  CHAT_DOCUMENTS = 'upload_files/chatDocuments/',
+}
+
+/*export enum UploadedFileDirectoryPath {
   TEMP = './public/upload_files/temp/',
   GLOBAL_GALLERY_CATEGORY = './public/upload_files/category/',
   GLOBAL_GALLERY_SUB_CATEGORY = './public/upload_files/subCategory/',
@@ -11,3 +25,4 @@ export enum UploadedFileDirectoryPath {
   GLOBAL_GALLERY_OTHERS = './public/upload_files/others/',
   CHAT_DOCUMENTS = './public/upload_files/chatDocuments/',
 }
+*/
