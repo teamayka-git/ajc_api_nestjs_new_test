916a4b593cbbfb0003f9b51669dc1c2079c66d20
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-04-06 20:42:29 +0530

    product module 4

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ModelNames } from 'src/common/model_names';
 import { ProductsSchema } from 'src/tableModels/products.model';
 import { ProductStoneLinkingsSchema } from 'src/tableModels/productStoneLinkings.model';
+import { CountersSchema } from 'src/tableModels/counters.model';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: ModelNames.PRODUCTS, schema: ProductsSchema },
+      { name: ModelNames.COUNTERS, schema: CountersSchema },
       {
         name: ModelNames.PRODUCT_STONE_LINKIGS,
         schema: ProductStoneLinkingsSchema,
