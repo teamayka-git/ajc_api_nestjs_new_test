7439886c98dd5e0d484d24e7a94272f0d0698dbf
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-08-13 03:17:06 +0530

    otp apis done 1

--- src/modules/otp/otp.module.ts
+++ src/modules/otp/otp.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { OtpService } from './otp.service';
 import { OtpController } from './otp.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ModelNames } from 'src/common/model_names';
+import { OtpSchema } from 'src/tableModels/otp.model';
+import { UserSchema } from 'src/tableModels/user.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ModelNames.OTP, schema: OtpSchema },
+      { name: ModelNames.USER, schema: UserSchema },
+    ]),
+  ],
   controllers: [OtpController],
   providers: [OtpService]
 })
