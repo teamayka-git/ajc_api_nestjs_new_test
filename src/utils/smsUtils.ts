46b619794e311f4fb874e2dfdbc6954fcaf29dd0
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-08-22 15:42:07 +0530

    shop api 5

--- src/utils/smsUtils.ts
+++ src/utils/smsUtils.ts
@@ -2,7 +2,7 @@ import { GlobalConfig } from "src/config/global_config";
 
 export class SmsUtils {
  async  sendSms(toNumber:String,smsContent:String)  {
-
+  try {
   console.log("___a1 "+toNumber);
 
     const twilioClient = require('twilio')(
@@ -11,13 +11,17 @@ export class SmsUtils {
     );
 
     console.log("___a2");
+
     var asdf = await twilioClient.messages.create({
       body: smsContent,
       messagingServiceSid: GlobalConfig().TWILIO_SMS_GATEWAY_SERVICE_SID,  
       to: '+91'+toNumber,
     });
     console.log("___a3 "+JSON.stringify(asdf) );
-    
+  } catch (error) {
+
+    console.log("Sms gateway error "+JSON.stringify(error));
+  }
   }
 
   
