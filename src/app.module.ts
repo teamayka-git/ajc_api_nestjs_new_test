c8dc38b4263e261a40a972aed0c1f28e76da96a7
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-10-22 15:39:16 +0530

    for interview

--- src/app.module.ts
+++ src/app.module.ts
@@ -223,6 +223,7 @@ export class AppModule {
         process.env.GLOBAL_PREFIX_FOR_API + '/generals/list',
         process.env.GLOBAL_PREFIX_FOR_API + '/otp/createOtp',
         process.env.GLOBAL_PREFIX_FOR_API + '/changeUserPassword',
+        process.env.GLOBAL_PREFIX_FOR_API + '/states/(.*)',
       )
       .forRoutes('*');
   }
