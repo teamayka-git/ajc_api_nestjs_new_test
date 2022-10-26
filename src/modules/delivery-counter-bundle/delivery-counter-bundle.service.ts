c691ddedf4a51f59ea42727ca2f0c9e565f467ec
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2022-10-26 15:38:58 +0530

    inv related before

--- src/modules/delivery-counter-bundle/delivery-counter-bundle.service.ts
+++ src/modules/delivery-counter-bundle/delivery-counter-bundle.service.ts
@@ -383,16 +383,7 @@ export class DeliveryCounterBundleService {
         );
       }
 
-      if (
-        dto.screenType.includes(116) &&
-        dto.invoiceUids.length == 0 &&
-        (dto.invoiceDateStartDate == -1 || dto.invoiceDateEndDate == -1)
-      ) {
-        throw new HttpException(
-          'inv related data not found, Bcz screenType contains 116',
-          HttpStatus.INTERNAL_SERVER_ERROR,
-        );
-      }
+      
 
       if (dto.deliveryBundleIds.length > 0) {
         var newSettingsId = [];
@@ -1089,38 +1080,12 @@ export class DeliveryCounterBundleService {
                       if (dto.screenType.includes(112)) {
                         const invoicePipeline = () => {
                           const pipeline = [];
-                          pipeline.push({
-                            $match: {
-                              $expr: { $eq: ['$_id', '$$invItemId'] },
-                            },
-                          });
-
-
-
-                          if (dto.invoiceUids.length != 0) {
-                            pipeline.push({
-                              $match: {
-                                _uid: { $in: dto.invoiceUids },
-                              },
-                            });
-                          }
-                          if (
-                            dto.invoiceDateStartDate != -1 &&
-                            dto.invoiceDateEndDate != -1
-                          ) {
-                            pipeline.push({
-                              $match: {
-                                _createdAt: {
-                                  $lte: dto.invoiceDateEndDate,
-                                  $gte: dto.invoiceDateStartDate,
-                                },
-                              },
-                            });
-                          }
-
-
-
                           pipeline.push(
+                            {
+                              $match: {
+                                $expr: { $eq: ['$_id', '$$invItemId'] },
+                              },
+                            },
                             new ModelWeightResponseFormat().invoiceTableResponseFormat(
                               1120,
                               dto.responseFormat,
@@ -1218,6 +1183,7 @@ export class DeliveryCounterBundleService {
                           {
                             $unwind: {
                               path: '$invoiceDetails',
+                              preserveNullAndEmptyArrays: true,
                             },
                           },
                         );
@@ -1238,6 +1204,7 @@ export class DeliveryCounterBundleService {
                       {
                         $unwind: {
                           path: '$invoiceItemDetails',
+                          preserveNullAndEmptyArrays: true,
                         },
                       },
                     );
