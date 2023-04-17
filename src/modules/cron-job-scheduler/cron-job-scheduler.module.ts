342bb8ae2abff6f0654bf431a5b67a082a9cc269
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2023-04-17 18:03:06 +0530

    cronjob test 2

--- src/modules/cron-job-scheduler/cron-job-scheduler.module.ts
+++ src/modules/cron-job-scheduler/cron-job-scheduler.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CronJobSchedulerServiceService } from './cron-job-scheduler-service.service';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
+  imports:[
+    ScheduleModule.forRoot()
+  ],
   controllers: [],
   providers: [CronJobSchedulerServiceService]
 })
