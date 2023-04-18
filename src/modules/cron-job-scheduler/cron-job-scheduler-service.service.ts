da68f3a70cab0bdbb4c99211351c3e64dcd91b16
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2023-04-18 16:03:02 +0530

    worker backlog count test 3

--- src/modules/cron-job-scheduler/cron-job-scheduler-service.service.ts
+++ src/modules/cron-job-scheduler/cron-job-scheduler-service.service.ts
@@ -211,6 +211,207 @@ export class CronJobSchedulerServiceService {
     }
   }
 
+
+
+  @Cron('5 16 * * *', {
+    timeZone: 'Asia/Kolkata',
+  })
+  async workerTodayDueAndBacklogCountSendCronJob() {
+    console.log('_____ cronjob workerTodayDueAndBacklogCountSendCronJob()');
+    var dateTime = new Date().getTime();
+    const transactionSession = await this.connection.startSession();
+    transactionSession.startTransaction();
+    try {
+      var result = await this.departmentModel.aggregate([
+        { $match: { _code: 1003, _status: 1 } },
+        {
+          $lookup: {
+            from: ModelNames.EMPLOYEES,
+            let: { departmentId: '$_id' },
+            pipeline: [
+              {
+                $match: {
+                  _status: 1,
+                  $expr: {
+                    $eq: ['$_departmentId', '$$departmentId'],
+                  },
+                },
+              },
+              {
+                $project: {
+                  _userId: 1,
+                },
+              },
+
+              {
+                $lookup: {
+                  from: ModelNames.USER,
+                  let: { userId: '$_userId' },
+                  pipeline: [
+                    {
+                      $match: {
+                        _status: 1,
+                        $expr: {
+                          $eq: ['$_id', '$$userId'],
+                        },
+                      },
+                    },
+                    {
+                      $project: {
+                        _id: 1,
+                        _name: 1,
+                        _isNotificationEnable: 1,
+                        _fcmId: 1,
+                      },
+                    },
+
+                    {
+                      $lookup: {
+                        from: ModelNames.ORDER_SALE_SET_PROCESSES,
+                        let: { userId: '$_id' },
+                        pipeline: [
+                          {
+                            $match: {
+                              _dueDate: {
+                                $lte: endOfDay(dateTime).getTime(),
+                                $gte: startOfDay(dateTime).getTime(),
+                              },
+                              _status: 1,
+                              _orderStatus: { $in: [1, 2] },
+                              $expr: {
+                                $eq: ['$_userId', '$$userId'],
+                              },
+                            },
+                          },
+                          {
+                            $project: {
+                              _id: 1,
+                            },
+                          },
+                          {
+                            $group: { _id: null, totalCount: { $sum: 1 } },
+                          },
+                        ],
+                        as: 'todaySetProcess',
+                      },
+                    },
+                    
+                    {
+                      $lookup: {
+                        from: ModelNames.ORDER_SALE_SET_PROCESSES,
+                        let: { userId: '$_id' },
+                        pipeline: [
+                          {
+                            $match: {
+                              _dueDate: {
+                                $lte: startOfDay(dateTime).getTime(),
+                              },
+                              _status: 1,
+                              _orderStatus: { $in: [1, 2] },
+                              $expr: {
+                                $eq: ['$_userId', '$$userId'],
+                              },
+                            },
+                          },
+                          {
+                            $project: {
+                              _id: 1,
+                            },
+                          },
+                          {
+                            $group: { _id: null, totalCount: { $sum: 1 } },
+                          },
+                        ],
+                        as: 'backlogSetProcess',
+                      },
+                    },
+                    {
+                      $match: {
+                        $or: [
+                          { todaySetProcess: { $ne: [] } },
+                          { backlogSetProcess: { $ne: [] } },
+                        ],
+                      },
+                    },
+                  
+                  ],
+                  as: 'userDetails',
+                },
+              },
+              {
+                $unwind: {
+                  path: '$userDetails',
+                },
+              },
+            ],
+            as: 'employeeDetails',
+          },
+        },
+      ]);
+      if (result.length != 0) {
+        for (var j = 0; j < result[0].employeeDetails.length; j++) {
+          //doing notification
+
+          var userFcmIds = [];
+          var userNotificationTable = [];
+          var notificationTitle = 'Set process due date report';
+          var notificationBody = `Your set process `;
+          if(result[0].employeeDetails[j].userDetail.todaySetProcess.length!=0){
+            notificationBody+=`${result[0].employeeDetails[j].userDetail.todaySetProcess.length} items due date is today and `;
+          }
+          if(result[0].employeeDetails[j].userDetail.backlogSetProcess.length!=0){
+            notificationBody+=`${result[0].employeeDetails[j].userDetail.backlogSetProcess.length} items already backlog`;
+          }
+          var notificationOrderSale = '';
+
+          if (
+            result[0].employeeDetails[j].userDetails._isNotificationEnable ==
+              1 &&
+            result[0].employeeDetails[j].userDetails._fcmId != ''
+          ) {
+            userFcmIds.push(result[0].employeeDetails[j].userDetails._fcmId);
+          }
+          userNotificationTable.push({
+            _viewStatus: 0,
+            _title: notificationTitle,
+            _body: notificationBody,
+            _orderSaleId:
+              notificationOrderSale == '' ? null : notificationOrderSale,
+            _userId: result[0].employeeDetails[j].userDetails._id,
+            _createdAt: dateTime,
+            _viewAt: 0,
+            _status: 1,
+          });
+
+          if (userNotificationTable.length != 0) {
+            await this.userNotificationModel.insertMany(userNotificationTable, {
+              session: transactionSession,
+            });
+          }
+          if (userFcmIds.length != 0) {
+            new FcmUtils().sendFcm(
+              notificationTitle,
+              notificationBody,
+              userFcmIds,
+              {
+                ajc: 'AJC_NOTIFICATION',
+              },
+            );
+          }
+          //done notification
+        }
+      }
+
+      await transactionSession.commitTransaction();
+      await transactionSession.endSession();
+      //   return responseJSON;
+    } catch (error) {
+      await transactionSession.abortTransaction();
+      await transactionSession.endSession();
+      throw error;
+    }
+  }
+
   @Cron('30 17 * * *', {
     timeZone: 'Asia/Kolkata',
   })
