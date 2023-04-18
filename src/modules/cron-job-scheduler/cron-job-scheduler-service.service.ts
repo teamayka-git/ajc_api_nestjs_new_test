c928a4a8c77ec0ced4d7b0f8cd756a3759c993e5
MOHAMMEDFAYIZALI+1-at-079136303911 <fayizalinp@gmail.com>
2023-04-18 12:05:39 +0530

    test cronjob 1

--- src/modules/cron-job-scheduler/cron-job-scheduler-service.service.ts
+++ src/modules/cron-job-scheduler/cron-job-scheduler-service.service.ts
@@ -1,12 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
 import { Cron } from '@nestjs/schedule';
+import { ModelNames } from 'src/common/model_names';
+import * as mongoose from 'mongoose';
+import { User } from 'src/tableModels/user.model';
+import { UserNotifications } from 'src/tableModels/user_notifications.model';
 
 @Injectable()
 export class CronJobSchedulerServiceService {
-    
+  constructor(
+    @InjectModel(ModelNames.USER)
+    private readonly userModel: mongoose.Model<User>,
+    @InjectModel(ModelNames.USER_NOTIFICATIONS)
+    private readonly userNotificationModel: mongoose.Model<UserNotifications>,
+    @InjectConnection() private readonly connection: mongoose.Connection,
+  ) {}
 
-    @Cron('0 0 * * *')
-    handleCron() {
-        console.log("_____ cronjob");
+  @Cron('* * * * *')
+  async handleCron() {
+    console.log('_____ cronjob');
+    var dateTime = new Date().getTime();
+    const transactionSession = await this.connection.startSession();
+    transactionSession.startTransaction();
+    try {
+      var arrayToUserNotifications = [];
+      arrayToUserNotifications.push({
+        _viewStatus: 0,
+        _title: 'From cronjob',
+        _body: 'From cronjob',
+        _orderSaleId: null,
+        _userId: null,
+        _viewAt: 0,
+        _createdAt: dateTime,
+        _status: 1,
+      });
+      await this.userNotificationModel.insertMany(arrayToUserNotifications, {
+        session: transactionSession,
+      });
+
+      await transactionSession.commitTransaction();
+      await transactionSession.endSession();
+      //   return responseJSON;
+    } catch (error) {
+      await transactionSession.abortTransaction();
+      await transactionSession.endSession();
+      throw error;
     }
+  }
 }
--- src/modules/cron-job-scheduler/cron-job-scheduler.module.ts
+++ src/modules/cron-job-scheduler/cron-job-scheduler.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { CronJobSchedulerServiceService } from './cron-job-scheduler-service.service';
 import { ScheduleModule } from '@nestjs/schedule';
+import { JwtModule } from '@nestjs/jwt';
+import { GlobalConfig } from 'src/config/global_config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ModelNames } from 'src/common/model_names';
+import { UserSchema } from 'src/tableModels/user.model';
+import { UserNotificationsSchema } from 'src/tableModels/user_notifications.model';
 
 @Module({
   imports:[
-    ScheduleModule.forRoot()
+    ScheduleModule.forRoot(),
+    JwtModule.register({
+      secret: GlobalConfig().JWT_SECRET_KEY,
+      signOptions: {},
+    }), //jwt implement
+    MongooseModule.forFeature([
+      { name: ModelNames.USER, schema: UserSchema },
+      { name: ModelNames.USER_NOTIFICATIONS, schema: UserNotificationsSchema },
+    ]),
+ 
   ],
   controllers: [],
   providers: [CronJobSchedulerServiceService]
