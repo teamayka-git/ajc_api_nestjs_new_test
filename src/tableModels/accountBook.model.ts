526b62e48ba79dcacb055c91ec9cec5834c44948
MOHAMED SAFEER <mohamed_safeer@gmail.com>
2023-08-14 17:04:27 +0530

    accountBook model changes

--- src/tableModels/accountBook.model.ts
+++ src/tableModels/accountBook.model.ts
@@ -5,8 +5,14 @@ import { GlobalConfig } from 'src/config/global_config';
 //safeer
 export const AccountBookSchema = new mongoose.Schema({
 
+
+  _transactionDate: { type: Number, required: true, default: -1 },
+  _voucherId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: ModelNames.ACCOUNT_VOUCHER,
+    default: null,
+  },
   _voucherNo: { type: String, required: true, default: 'nil' },
-  _voucherDate: { type: Number, required: true, default: -1 },
   _voucherType: { type: Number, required: true, default: 1 },
   _ledgerId: {
     type: mongoose.Schema.Types.ObjectId,
@@ -19,8 +25,6 @@ export const AccountBookSchema = new mongoose.Schema({
     default: null,
   },
   _description: { type: String, required: false, default: '' },
-  _docNo: { type: String, required: false, default: '' },
-  _docDate: { type: Number, required: false, default: -1 },
   _crdr: { type: Number, required: true, default: 1 },
   _amount: { type: Number, required: true, default: 0 },
   _total: { type: Number, required: true, default: 0 },
